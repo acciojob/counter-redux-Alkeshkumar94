02a0d4b4414bbf3c93f8edaad0b195cc6a62fd84
Alkeshkumar94 <alkeshkumar949@gmail.com>
2024-08-04 11:38:41 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-
 import React from "react";
 import './../styles/App.css';
-
+import { useDispatch } from "react-redux";
+import Count from "./count"
 const App = () => {
+  const dispatch=useDispatch();
   return (
     <div>
-        {/* Do not remove the main div */}
+       <button onClick={()=>dispatch({type:"Decrement"})}>-</button>
+       <Count/>
+       <button onClick={()=>dispatch({type:"Increment"})}>+</button>
     </div>
   )
-}
+};
 
 export default App
